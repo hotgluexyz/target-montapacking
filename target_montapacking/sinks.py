f7898e2d002e31a1ae19b73deb8127f189412f43
arthurto <arthur.ps22009@gmail.com>
2022-11-30 15:05:09 -0300

    initial commit

---
+++ target_montapacking/sinks.py
@@ -0,0 +1,15 @@
+"""Montapacking target sink class, which handles writing streams."""
+
+from __future__ import annotations
+
+from singer_sdk.sinks import RecordSink
+
+
+class MontapackingSink(RecordSink):
+    """Montapacking target sink class."""
+
+    def process_record(self, record: dict, context: dict) -> None:
+        """Process the record."""
+        # Sample:
+        # ------
+        # client.write(record)
