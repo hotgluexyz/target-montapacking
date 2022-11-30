563fceb27b3a0f691d6d06b689d6e993aaabe5eb
arthurto <arthur.ps22009@gmail.com>
2022-11-30 19:40:12 -0300

    SalesOrders working

--- target_montapacking/client.py
+++ target_montapacking/client.py
@@ -1,4 +1,4 @@
-"""WoocommerceSink target sink class, which handles writing streams."""
+"""MontapackingSink target sink class, which handles writing streams."""
 
 from typing import Any, Callable, Dict, List, Optional, cast
 
@@ -44,28 +44,3 @@ class MontapackingSink(RecordSink, Rest):
         resp = self.request_api("GET", endpoint=endpoint)
         return resp.json()
 
-    # def get_reference_data(self, stream, fields=None, filter={}):
-    #     page = 1
-    #     data = []
-    #     params = {"per_page": 100, "order": "asc", "page": page}
-    #     params.update(filter)
-    #     while True:
-    #         resp = self.request_api("GET", stream, params)
-    #         total_pages = resp.headers.get("X-WP-TotalPages")
-    #         resp = resp.json()
-    #         if fields:
-    #             resp = [{k:v for k, v in r.items() if k in fields} for r in resp]
-    #         data += resp
-
-    #         if resp and int(total_pages) > page:
-    #             page += 1
-    #             params.update({"page": page})
-    #         else:
-    #             break
-    #     return data
-
-    # def process_record(self, record: dict, context: dict) -> None:
-    #     """Process the record."""
-    #     response = self.request_api("POST", request_data=record)
-    #     # id = response.json().get("id")
-    #     # self.logger.info(f"{self.name} created with id: {id}")
